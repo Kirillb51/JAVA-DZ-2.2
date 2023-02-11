de1abf9e7400294c3041646011c7c6f8ad576daa
Kirill Igorevich <k.beketoff@yandex.ru>
2023-02-11 14:10:22 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 300;
-        int y = 2000;
+        int account = 300;
+        int replenishment = 2000;
         int bonus = 1;
-        if (y > 1000) {
-            System.out.println(x + y + (y / 100));
+        boolean replenisment = true;
+        if (replenishment > 1000) {
+            System.out.println(account + replenishment + (replenishment / 100));
             //Бонус при пополнении счета на сумму 2000= 20 бонусным рублям,при пополнении
             // счета на сумму менее 1000 расчёт бонусов не производится.
         }
